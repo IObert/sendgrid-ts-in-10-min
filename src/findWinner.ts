2fff2d0e204ac5c65994c6ba5dd5bc365dfba6fb
Marius Obert <obatzer@gmail.com>
2022-03-04 12:45:40 +0100

    Add solution

---
+++ src/findWinner.ts
@@ -0,0 +1,53 @@
+import { MailService } from "@sendgrid/mail";
+import { readFileSync } from "fs";
+import { email } from "datamask";
+
+const SOLUTION = +(process.env.SOLUTION || "0"); // later add solution
+
+const rawEntries = readFileSync("entries.txt").toString();
+const dedupedEntries: { [key: string]: Entry } = {};
+
+rawEntries.split("\n").forEach((line) => {
+  const emailAddress = line.split("# #")[0],
+    estimate = +line.split("# #")[1];
+  if (emailAddress) {
+    dedupedEntries[emailAddress] = {
+      email: emailAddress,
+      estimate: estimate,
+      diff: Math.abs(SOLUTION - estimate),
+    };
+  }
+});
+
+let entries = Object.values(dedupedEntries);
+
+const ranking = entries
+  .sort((a, b) => a.diff - b.diff)
+  .map((entry, idx) => `${idx + 1}.\t${email(entry.email)}\t${entry.estimate}`)
+  .join("\n");
+
+const sendgridClient = new MailService();
+sendgridClient.setApiKey(process.env.SENDGRID_API_KEY || "");
+
+entries.forEach(async (entry) => {
+  try {
+    await sendgridClient.send({
+      to: {
+        email: entry.email,
+      },
+      from: {
+        email: "lottery@zero-g.me",
+        name: "SendGrid Demo",
+      },
+      templateId: "d-5b4125d183f5460cb8247c06d619c25d",
+      dynamicTemplateData: {
+        ranking,
+        answer: process.env.SOLUTION,
+      },
+    });
+    console.log(`Notified ${email(entry.email)}`);
+  } catch (e) {
+    console.error(e);
+    debugger;
+  }
+});
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,58 @@
 import fastify, { FastifyInstance } from "fastify";
+import { appendFileSync } from "fs";
+import fastifyMultipart from "fastify-multipart";
+import { MailService } from "@sendgrid/mail";
+import { email } from "datamask";
+
+const sendgridClient = new MailService();
+
+sendgridClient.setApiKey(process.env.SENDGRID_API_KEY || "");
 
 const server: FastifyInstance = fastify({});
 
 server
+  .register(fastifyMultipart, { addToBody: true })
   .all("/hello", async (_, reply) => {
     reply.send({ hello: "world" });
-  });
+  })
+  .all("/mail", async (request, reply) => {
+    const sgBody = request.body as EmailBody;
+    const regExFloat = /([0-9]*[.])?[0-9]+/g;
+   
+    const guess = sgBody.text.match(regExFloat)[0];
+    const sender = JSON.parse(sgBody.envelope).from;
+   
+    appendFileSync("entries.txt", `${sender}# #${guess}\n`);
+   
+    const msg = {
+      to: {
+        email: sender,
+      },
+      from: {
+        email: "lottery@zero-g.me",
+        name: "SendGrid Demo",
+      },
+      subject: `RE: ${sgBody.subject}`,
+      html: `<h1>Thanks for your Submission!</h1>
+      <p>We registered your guess "${guess}" based on the message you sent:</p>
+      <p style="margin-left:10%; margin-right:10%;font-style: italic;">${sgBody.text}</p>
+   
+      <p>Feel free to send another email if you want to update your guess.</p>
+   
+      <p>The submitted data will be only be used for the demo and deleted afterward.</p>`,
+    };
+   
+    try {
+      await sendgridClient.send(msg);
+      console.log(`Response has been sent to ${email(sender)}.`);
+      reply.code(201);
+    } catch (error) {
+      console.error(error);
+      reply.code(500);
+    }
+   
+    reply.send();
+   });
 
 server.listen(3000, function (err, address) {
   if (err) {
